9289b3d63825f0bc072508cb0f3bb4c467324a4e
Shyronzo <james-sst@hotmail.com>
2023-02-24 15:02:12 +0100

    adding Elements to the cursed limbs

--- Assets/Scripts/CursedScripts/CurseController.cs
+++ Assets/Scripts/CursedScripts/CurseController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CurseController : MonoBehaviour
 {
-    public int  CurseCount, CursedLimbCount, PrevLimbInt, SecondLimbCheck;
+    public int  CurseCount, CursedLimbCount, PrevLimbInt, SecondLimbCheck, ElementalRando;
     public int[] CursedLimbs, WhatCurse;
-    public bool FinalCurseCheck;
-    public LimbElement Allignment;
+    public bool FinalCurseCheck, CheckFinished;
+    public GameObject[] Limbs;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +28,7 @@ public class CurseController : MonoBehaviour
         {
 
             CursedLimbs[CursedLimbCount] = Random.Range(0, 6);
+            
             PrevLimbInt = CursedLimbs[CursedLimbCount];
             CursedLimbCount++;
 
@@ -51,7 +53,110 @@ public class CurseController : MonoBehaviour
                     }
                 }
             }
+            CheckFinished = true; 
+           
         }
-       
+        foreach (GameObject Limb in Limbs)
+        {
+            for (var i = 0; i < CurseCount; i++)
+            {
+                for (var j = 0; j < CurseCount; j++)
+                {
+                    if (i != j)
+                    {
+                        if (Limbs[i] == Limbs[j])
+                        {
+                            ElementalRando++;
+                            if(ElementalRando>6) ElementalRando = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    void ElementalLimb()
+    {
+        for (var i = 0; i < CurseCount; i++)
+        {
+            ElementalRando = Random.Range(0, 6);
+            switch (ElementalRando)
+            {
+                case 0:
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Void = true;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Fire = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Light = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Dark = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Wind = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Water = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Ice = false;
+                    return;
+
+                case 1:
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Void = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Fire = true;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Light = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Dark = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Wind = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Water = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Ice = false;
+                    return;
+
+                case 2:
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Void = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Fire = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Light = true;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Dark = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Wind = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Water = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Ice = false;
+                    return;
+
+                case 3:
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Void = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Fire = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Light = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Dark = true;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Wind = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Water = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Ice = false;
+                    return;
+
+                case 4:
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Void = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Fire = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Light = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Dark = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Wind = true;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Water = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Ice = false;
+                    return;
+
+                case 5:
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Void = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Fire = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Light = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Dark = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Wind = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Water = true;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Ice = false;
+                    return;
+
+                case 6:
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Void = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Fire = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Light = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Dark = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Wind = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Water = false;
+                    Limbs[CursedLimbs[i]].GetComponent<LimbElement>().Ice = true;
+                    return;
+
+            }
+
+        }
+
+
     }
 }
