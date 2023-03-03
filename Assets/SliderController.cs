ad60242e345251b4367ff9dc94444676dc8ab513
madelino22 <62880766+madelino22@users.noreply.github.com>
2023-03-03 12:03:31 +0100

    Update SliderController.cs

--- Assets/SliderController.cs
+++ Assets/SliderController.cs
@@ -7,15 +7,15 @@ using TMPro;
 public class SliderController : MonoBehaviour
 {
 
-    enum SliderState{OFF, MOVING, FINISHED}
-    
-    [SerializeField]float successPoint = 0.5f;
-    [SerializeField]float successMargin = 0.1f;
+    enum SliderState { OFF, MOVING, FINISHED }
+
+    [SerializeField] float successPoint = 0.5f;
+    [SerializeField] float successMargin = 0.1f;
     [SerializeField] float speed;
     [SerializeField] GameObject textGO;
     [SerializeField] GameObject background;
-    
-    
+
+
     Slider slider;
     SliderState state = SliderState.OFF;
 
@@ -26,11 +26,33 @@ public class SliderController : MonoBehaviour
     void Start()
     {
         slider = GetComponent<Slider>();
-        
+
         background.GetComponent<Image>().material.SetFloat("_SuccessNumber", successPoint);
         background.GetComponent<Image>().material.SetFloat("_SuccessMargin", successMargin);
     }
 
+
+    private void OnEnable()
+    {
+        state = SliderState.OFF;
+        background.GetComponent<Image>().material.SetFloat("_SuccessNumber", successPoint);
+        background.GetComponent<Image>().material.SetFloat("_SuccessMargin", successMargin);
+
+    }
+
+
+    private IEnumerator SwitchStateTimer(SliderState newState, float waitingTime)
+    {
+        yield return new WaitForSeconds(waitingTime);
+        state = newState;
+        if (state == SliderState.OFF)
+        {
+            state = SliderState.OFF;
+            textGO.SetActive(false);
+            slider.value = 0;
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
@@ -50,17 +72,26 @@ public class SliderController : MonoBehaviour
                     state = SliderState.FINISHED;
 
                     textGO.SetActive(true);
+                    bool successBool = true;
                     //activate text saying things and check how good it is
-                    if(slider.value >= successPoint - successMargin && slider.value <= successPoint + successMargin)
+                    if (slider.value >= successPoint - successMargin && slider.value <= successPoint + successMargin)
                     {
                         textGO.GetComponent<TextMeshProUGUI>().text = "SUCCESS";
                         textGO.GetComponent<TextMeshProUGUI>().color = Color.green;
+                        successBool = true;
                     }
                     else
                     {
                         textGO.GetComponent<TextMeshProUGUI>().text = "FAIL";
                         textGO.GetComponent<TextMeshProUGUI>().color = Color.red;
+                        successBool = false;
                     }
+
+                    //invoke an extern method to tell the caller the result
+
+                    //Create a timer for swithing to FINISHED state automatically
+                    StartCoroutine(SwitchStateTimer(SliderState.OFF, 2f));
+
                 }
                 break;
             case SliderState.FINISHED:
