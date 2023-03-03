629b424ccdd5f6730ada8e5db067599b7fecc92a
Epicdragonbro <73120848+Epicdragonbro@users.noreply.github.com>
2023-03-03 13:19:41 +0100

    transfer from main

--- Assets/Scripts/Ingredients/Ingredient.cs
+++ Assets/Scripts/Ingredients/Ingredient.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 //using UnityEngine.XR.Interaction.Toolkit;
 
 public enum Elements { Fire, Dark, Light, Water, Air, Earth, None }
 //[RequireComponent(typeof(XRGrabInteractable), typeof(Renderer), typeof(Rigidbody))]
+//[RequireComponent(typeof(ParticleSystem))]
 public class Ingredient : MonoBehaviour
 {
     //public bool Burned, Gassed, Drenched, Dusted, Shining, Darkened;
@@ -16,6 +19,10 @@ public class Ingredient : MonoBehaviour
     //XRBaseInteractable m_Interactable;
     //Renderer m_Renderer;
     Rigidbody rb;
+    //ParticleSystem PS;
+    //ParticleSystemRenderer PSR;
+    ParticleSystemShapeType IngredientMesh = ParticleSystemShapeType.MeshRenderer;
+    public Material FireSpell, AirSpell, WaterSpell, EarthSpell, LightSpell, DarkSpell;
 
     public int strength;
     public Elements element;
@@ -26,49 +33,58 @@ public class Ingredient : MonoBehaviour
         element = Elements.None;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider collision)
     {
         //if (!Burned && !Gassed && !Drenched && !Dusted && !Shining && !Darkened)
         //{
-        if (element == Elements.None)
-        {
-            switch (other.gameObject.tag)
+        if (element==Elements.None){
+            switch (collision.gameObject.tag)
             {
                 case "Spell/Fire":
                     Debug.Log(this.name + " was hit by Fire");
                     //Burned= true;
                     element = Elements.Fire;
-                    StartParticles(Color.red);
+                    StartParticles(collision.gameObject.GetComponent<MeshRenderer>().material);
                     break;
 
                 case "Spell/Air":
                     Debug.Log(this.name + " was hit by Air");
                     //Gassed = true;
                     element = Elements.Air;
+                    //StartParticles(AirSpell);
+                    StartParticles(collision.gameObject.GetComponent<MeshRenderer>().material);
                     break;
 
                 case "Spell/Water":
                     Debug.Log(this.name + " was hit by Water");
                     //Drenched = true;
                     element = Elements.Water;
+                    //StartParticles(WaterSpell);
+                    StartParticles(collision.gameObject.GetComponent<MeshRenderer>().material);
                     break;
 
                 case "Spell/Earth":
                     Debug.Log(this.name + " was hit by Earth");
                     //Dusted= true;
                     element = Elements.Earth;
+                    //StartParticles(EarthSpell);
+                    StartParticles(collision.gameObject.GetComponent<MeshRenderer>().material);
                     break;
 
                 case "Spell/Light":
                     Debug.Log(this.name + " was hit by Light");
                     //Shining= true;
                     element = Elements.Light;
+                    //StartParticles(LightSpell);
+                    StartParticles(collision.gameObject.GetComponent<MeshRenderer>().material);
                     break;
 
                 case "Spell/Dark":
                     Debug.Log(this.name + " was hit by Dark");
                     //Darkened= true;
                     element = Elements.Dark;
+                    //StartParticles(DarkSpell);
+                    StartParticles(collision.gameObject.GetComponent<MeshRenderer>().material);
                     break;
 
                 default:
@@ -80,8 +96,16 @@ public class Ingredient : MonoBehaviour
     }
     //}
 
-    void StartParticles(Color color)
+    void StartParticles(Material ElementHit)
     {
+        Debug.Log(gameObject.name + " has " + ElementHit.name);
+        ParticleSystem PS = gameObject.AddComponent<ParticleSystem>();
+        ParticleSystemRenderer PSR = gameObject.GetComponent<ParticleSystemRenderer>();
+        PSR.material= ElementHit;
+        //PS.startColor = color;
+        //PSR.material= ElementHit;
+        //PS.Play();
+        //PS.shape.meshRenderer = gameObject.GetComponent<MeshRenderer>();
 
     }
 
