f435a827543a61a1de54661bcb2a5bd8a818733a
Epicdragonbro <73120848+Epicdragonbro@users.noreply.github.com>
2023-03-10 14:29:59 +0100

    Basic Menu implementation

---
+++ Assets/Scripts/MenuFunctionality.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFunctionality : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        // save any game data here
+        #if UNITY_EDITOR
+                // Application.Quit() does not work in the editor so
+                // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                 Application.Quit();
+        #endif
+    }
+}
