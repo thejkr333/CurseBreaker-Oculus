edd43de22c8cbecfcd93fb444084cb01716c7ecd
Shyronzo <james-sst@hotmail.com>
2023-03-11 15:44:42 +0100

    Implementing Height Change very rough
    default oculus menu button, LEFT HAND :ok_hand: to open and show height adjustment

---
+++ Assets/Scripts/HeightSlider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.XR.LegacyInputHelpers;
+using UnityEngine;
+using UnityEngine.UI;
+public class HeightSlider : MonoBehaviour
+{
+    public GameObject CameraRig;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRig.transform.position = new Vector3(CameraRig.transform.position.x, slider.value, CameraRig.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/SliderAdjustmentButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderAdjustmentButton : MonoBehaviour
+{
+    public Slider Slider;
+    public bool Increase, Decrease;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Increase) IncreaseSlider();
+        if(Decrease) DecreaseSlider();
+    }
+    public void IncreaseSlider()
+    {
+        Slider.value += 0.0001f;
+    }
+    public void DecreaseSlider()
+    {
+        Slider.value -= 0.0001f;
+    }
+}
