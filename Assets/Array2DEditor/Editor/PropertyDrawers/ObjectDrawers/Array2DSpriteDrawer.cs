9cca61b91c0f652bd2f0fb36ab5600219a266459
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2023-03-24 12:51:09 +0100

    deleting package

--- Assets/Array2DEditor/Editor/PropertyDrawers/ObjectDrawers/Array2DSpriteDrawer.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEditor;
-
-namespace Array2DEditor
-{
-    [CustomPropertyDrawer(typeof(Array2DSprite))]
-    public class Array2DSpriteDrawer : Array2DObjectDrawer<Sprite> { }
-}
