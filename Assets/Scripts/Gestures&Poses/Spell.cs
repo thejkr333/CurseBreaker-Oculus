39729ac52383ef502316e88f1cd6dd76eb03e40c
Shyronzo <james-sst@hotmail.com>
2023-03-24 16:16:47 +0100

    reenabling old spell behaviour
    making the spell stick to the hand without the grab, should hopefully actually apply the effect to the ingredient first before destroying itself, as they are no longer "grabbed" and cant break the game. just turn the script on and comment out the destroy to reenable the grabbing part.

--- Assets/Scripts/Gestures&Poses/Spell.cs
+++ Assets/Scripts/Gestures&Poses/Spell.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public class Spell : MonoBehaviour
 { public GameObject FirstHand;
-    private float deleteTimer = 10;
+   // private float deleteTimer = 10;
     private bool removeSpell;
+    public bool testDebug;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,40 +17,40 @@ public class Spell : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (deleteTimer < 0 && removeSpell == true)
+        if (/*deleteTimer < 0 &&*/ removeSpell == true&&testDebug == false)
         {
             Destroy(gameObject);
         }
         if(removeSpell== true)
         {
-            deleteTimer-= Time.deltaTime;
+           /* deleteTimer-= Time.deltaTime; */
         }
 
-        if (FirstHand.GetComponent<PoseGrab>().SpellRelease == true)
-        {
-            removeSpell = true;
-            
-        }
-        if (FirstHand.GetComponent<PoseGrab>().SpellRelease == false)
-            removeSpell = false;
+        transform.position = FirstHand.transform.position;
+
     }
-    private void OnTriggerStay(Collider other)
-    {
-        deleteTimer = 10;
-    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (FirstHand != null && other.gameObject != FirstHand)
         {
 
+            // it now checks if it cant find the grabbable script, so effectively an ingredient or other object, then if it doesnt see that it destroys itself, meaning at most it should apply the effect to the ingredient before destroying itself
+
+            if(other.gameObject.GetComponent<OVRGrabbable>() == null)
+            { //this is here just to stop the errors and prevent them from happening
+
+
+                //Errors out if the player is still trying to grab the spell, doing any other pose before it is destroyed prevents it erroring out 
+
+                //Reenabling this bit and testing how it might work if you cant grab it and it follows the hand directly, no grab needed
+                Destroy(gameObject);
+            }
            // FirstHand.GetComponent<PoseGrab>().SpellRelease = true;
 
 
-           //this is here just to stop the errors and prevent them from happening
           
-            
-            //Errors out if the player is still trying to grab the spell, doing any other pose before it is destroyed prevents it erroring out 
-            //Destroy(gameObject);
+           
         }
         if (FirstHand == null)
         {
@@ -70,10 +71,19 @@ public class Spell : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject == FirstHand)
+        /*if(other.gameObject == FirstHand)
         {
            // removeSpell = true;
             deleteTimer = 0.1f;
         }
+        if (FirstHand.GetComponent<PoseGrab>().SpellRelease == true)
+        {
+            removeSpell = true;
+
+        }
+        if (FirstHand.GetComponent<PoseGrab>().SpellRelease == false)
+        {
+            removeSpell = false;
+        }*/
     }
 }
