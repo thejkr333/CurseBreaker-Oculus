544a1f1b967764b388b1376655a1813532c43d91
BrailleBell <Bellingmo.iver@gmail.com>
2023-04-14 13:01:59 +0200

    scroll object
    been working on the scroll

---
+++ Assets/Scripts/scroll/scroll_info.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new_scroll",menuName = "scroll")]
+public class scroll_info : ScriptableObject
+{
+    public Sprite previewImage;
+    public string previewString;
+}
