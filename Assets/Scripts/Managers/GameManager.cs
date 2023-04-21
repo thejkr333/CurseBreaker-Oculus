c740aa7099a2ab7481cc5924992529345fd21e4d
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2023-04-21 10:09:18 +0200

    fixing load and save functions

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -16,13 +17,13 @@ public class GameManager : MonoBehaviour
     const int NUMBEROFCUSTOMERSPERDAY = 3;
     [SerializeField] GameObject[] customersToday = new GameObject[NUMBEROFCUSTOMERSPERDAY];
 
-    int dayCount;
+    [SerializeField] int dayCount;
 
     public event Action OnNewDay;
     public event Action<Ingredients[]> CreateShop;
 
     //True == unlocked ---- false == locked
-    Dictionary<Curses, bool> cursesLockInfo = new Dictionary<Curses, bool>();
+    Dictionary<Curses, bool> cursesLockInfo = new();
 
     private void Awake()
     {
@@ -35,20 +36,30 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
 
+        this.OnNewDay += NewDay;
+
         //Populate the dictionary putting all the curses as locked
         foreach (Curses curse in Enum.GetValues(typeof(Curses)))
         {
             cursesLockInfo.Add(curse, false);
         }
 
+        foreach(var info in cursesLockInfo)
+        {
+            Debug.Log("Key: " + info.Key + " value: " + info.Value);        
+        }
+
         //Starting unlocked curses
         cursesLockInfo[Curses.Wolfus] = true;
         cursesLockInfo[Curses.Gassle] = true;
 
-        LoadGame();
+        if(PlayerPrefs.HasKey("DayCount")) LoadGame();
+        else OnNewDay?.Invoke();
+    }
 
-        this.OnNewDay += NewDay;
-        OnNewDay?.Invoke();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) SaveGame();
     }
 
     public void RentIncrease()
@@ -109,6 +120,8 @@ public class GameManager : MonoBehaviour
 
     void SaveGame()
     {
+        Debug.Log("Save");
+
         // Save desired stats from PlayerPrefs
         PlayerPrefs.SetInt("DayCount", dayCount);
         PlayerPrefs.SetInt("Gold", Gold);
@@ -124,9 +137,22 @@ public class GameManager : MonoBehaviour
         // Load desired stats from PlayerPrefs
         dayCount = PlayerPrefs.GetInt("DayCount", 0);
         Gold = PlayerPrefs.GetInt("Gold", 0);
-        foreach (var curse in cursesLockInfo)
+
+        //for (int i = 0; i < cursesLockInfo.Count; i++)
+        //{
+        //    cursesLockInfo[(Curses)i] = PlayerPrefs.GetInt(((Curses)i).ToString()) == 1 ? true : false;
+        //}
+
+        //Dictionary<Curses, bool> _copyInfo = new(cursesLockInfo);
+        //foreach (var info in _copyInfo)
+        //{
+        //    cursesLockInfo[info.Key] = PlayerPrefs.GetInt(info.Key.ToString()) == 1 ? true : false;
+        //}
+
+
+        foreach (var infoKey in cursesLockInfo.Keys.ToList())
         {
-            cursesLockInfo[curse.Key] = PlayerPrefs.GetInt(curse.Key.ToString()) == 1 ? true : false;
+            cursesLockInfo[infoKey] = PlayerPrefs.GetInt(infoKey.ToString()) == 1 ? true : false;
         }
     }
 }
