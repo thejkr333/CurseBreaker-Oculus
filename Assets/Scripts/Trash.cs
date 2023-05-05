e0b6fa5bbf2d8519436ebe6774537d17d3019030
Shyronzo <james-sst@hotmail.com>
2023-05-05 18:20:21 +0200

    making the trash foolproof in theory
    along with an added item respawn area

--- Assets/Scripts/Trash.cs
+++ Assets/Scripts/Trash.cs
@@ -6,8 +6,13 @@ public class Trash : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.TryGetComponent(out OVRGrabbable grabbable)) return;
+        /*if (!other.TryGetComponent(out OVRGrabbable grabbable)) return;
         
-        Destroy(other.gameObject);
+        Destroy(other.gameObject);*/
+        if (other.TryGetComponent(out Trashable trashable))
+        {
+            if(other.GetComponent<Trashable>().trashable == true) Destroy(other.gameObject);
+        }
+        
     }
 }
