02cd47e86173462ec805eb4770190006ad865fc1
Shyronzo <james-sst@hotmail.com>
2023-05-12 15:38:24 +0200

    moving some scripts to the unused folder and adding collider for table
    means things can spawn on there if they fall out of the room for whatever reason

--- Assets/Scripts/Managers/SunController.cs
+++ Assets/Scripts/Managers/SunController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
@@ -8,6 +8,7 @@ public class SunController : MonoBehaviour
     public int CurrentCustomer;
     public int[] XRotForSun;
     public GameObject Sun;
+    public float XRotAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +23,24 @@ public class SunController : MonoBehaviour
         {
             CurrentCustomer = 2;
         }
+        if (CurrentCustomer == 0) Sun.GetComponent<Light>().color = Color.yellow;
+        if(CurrentCustomer == 1) Sun.GetComponent<Light>().color = Color.white;
+        if( CurrentCustomer == 2) Sun.GetComponent<Light>().color = Color.black;
+        if (GameManager.Instance.NextDayAnimation == true)
+        {
+            Sun.transform.rotation = Quaternion.Euler(XRotAnim, 0, 0);
+            
+            XRotAnim += Time.deltaTime*10;
+            if (XRotAnim >= 400)
+            {
 
+
+                GameManager.Instance.NextDayAnimation = false;
+            }
+            goto SkipFlicker;
+        }
+        if (GameManager.Instance.NextDayAnimation == false) XRotAnim = 330;
         Sun.transform.rotation = Quaternion.Euler(XRotForSun[CurrentCustomer], 0, 0);
+    SkipFlicker:;
     }
 }
