22dd157511faf75e543dee795a09f4bb2a54aa6b
Epicdragonbro <73120848+Epicdragonbro@users.noreply.github.com>
2023-05-29 14:28:50 +0200

    Lizard Tail added to bottom shelf

--- Assets/Scripts/Enviroment/FloatInBottle.cs
+++ Assets/Scripts/Enviroment/FloatInBottle.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class FloatInBottle : MonoBehaviour
 {
-    public float LerpSpeed = 1, BaseDistance = 0.1f;
-    public float MinLerpSpeed = .1f, MaxLerpSpeed = 1, MinBaseDistance = .1f, MaxBaseDistance = .2f;
+    public float LerpSpeed = 1, BaseDistance = 0.1f, SpinSpeed = 1f;
+    public float MinLerpSpeed = .1f, MaxLerpSpeed = 2, MinBaseDistance = .004f, MaxBaseDistance = .015f, MinSpinSpeed = .25f, MaxSpinSpeed = .5f;
 
     Vector3 startPos, currentPos; //endPos;
 
-    public bool DoLerp = true;
+    public bool DoLerp = true, DoSpin = false, InverseSpin = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +19,7 @@ public class FloatInBottle : MonoBehaviour
         currentPos = startPos;
         BaseDistance = Random.Range(MinBaseDistance, MaxBaseDistance);
         LerpSpeed = Random.Range(MinLerpSpeed, MaxLerpSpeed);
+        SpinSpeed = Random.Range(MinSpinSpeed, MaxSpinSpeed);
     }
 
     // Update is called once per frame
@@ -28,6 +29,18 @@ public class FloatInBottle : MonoBehaviour
         {
             currentPos.y = startPos.y + Mathf.Sin(Time.time * LerpSpeed) * BaseDistance; //Mathf.PingPong(LerpSpeed*Time.time, LerpDistance);//Mathf.SmoothStep(startPos.y, endPos.y, LerpSpeed * Time.time);
             transform.localPosition = currentPos;
+        }
+
+        if (DoSpin)
+        {
+            if (InverseSpin)
+            {
+                gameObject.transform.Rotate(0f, -SpinSpeed, 0f);
+            }else
+            {
+                gameObject.transform.Rotate(0f, SpinSpeed, 0f);
+
+            }
         }
     }
 }
